e8fbaa0f3d29d1af84d11a9c6c9393460c555391
Denys Podobrii <denys.podobrii@gmail.com>
2025-06-15 15:59:29 +0300

    Change base URL

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const axiosMovieInstance = axios.create({
+export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_MOVIE_URL,
     headers: {
         'accept': 'application/json',
     },
 })
 
-axiosMovieInstance.interceptors.request.use((request) => {
+axiosInstance.interceptors.request.use((request) => {
     request.headers.Authorization = 'Bearer ' + import.meta.env.VITE_API_AUTH_TOKEN;
     return request;
 })
