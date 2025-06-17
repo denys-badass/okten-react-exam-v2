e2475cb2eeaad098f0ec04df814a2afccdf62dfc
Denys Podobrii <denys.podobrii@gmail.com>
2025-06-17 21:43:57 +0300

    handle global error in axios

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -11,3 +11,10 @@ axiosInstance.interceptors.request.use((request) => {
     request.headers.Authorization = 'Bearer ' + import.meta.env.VITE_API_AUTH_TOKEN;
     return request;
 })
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error("Error: " + error.message);
+        return Promise.reject(error);
+    });
