bc43d21e25e98a365187a417ae55587cefe49ecf
Denys Podobrii <denys.podobrii@gmail.com>
2025-06-29 10:12:47 +0300

    change styles on toggle

--- src/components/toggle-theme/ToggleTheme.tsx
+++ src/components/toggle-theme/ToggleTheme.tsx
@@ -7,7 +7,7 @@ export const ToggleTheme = () => {
     return (
         <>
             <div
-                className='rounded-[50%] w-[30px] h-[26px] bg-slate-100 dark:bg-slate-800 flex items-center justify-center'
+                className='rounded-[50%] w-[30px] h-[26px] bg-slate-100 dark:bg-slate-800 flex items-center justify-center cursor-pointer hover:bg-indigo-800 hover:text-gray-50'
                 onClick={toggleTheme}>
                 {
                     theme === 'light' ? <MdOutlineLightMode/> : <MdDarkMode/>
