000ba28b40eea75402df248db6d6c3dffef7cbc8
Denys Podobrii <denys.podobrii@gmail.com>
2025-07-09 17:40:39 +0300

    add redirect to login

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,12 +4,13 @@ import {WelcomePage} from "../pages/WelcomePage.tsx";
 import {MoviesPage} from "../pages/MoviesPage.tsx";
 import {SearchPage} from "../pages/SearchPage.tsx";
 import {LoginPage} from "../pages/LoginPage.tsx";
+import {LoginRoute} from "../components/login-route/LoginRoute.tsx";
 
 export const routes = createBrowserRouter([
     {path: '/', element: <MainLayout/>, children: [
             {index: true, element: <WelcomePage/>},
-            {path: 'movies', element: <MoviesPage/>},
-            {path: 'search', element: <SearchPage/>},
+            {path: 'movies', element: <LoginRoute><MoviesPage/></LoginRoute>},
+            {path: 'search', element: <LoginRoute><SearchPage/></LoginRoute>},
             {path: 'login', element: <LoginPage/>}
         ]},
 ])
