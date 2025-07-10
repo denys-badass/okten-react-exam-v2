2502abcef90970109f4520c2a61f60c73d490110
Denys Podobrii <denys.podobrii@gmail.com>
2025-07-10 18:32:51 +0300

    add User info drop down menu

--- src/components/header/user-avatar/UserAvatar.tsx
+++ src/components/header/user-avatar/UserAvatar.tsx
@@ -1,5 +1,6 @@
-import type {FC} from "react";
+import {type FC, useState} from "react";
 import type {IUser} from "../../../models/IUser.ts";
+import {UserInfo} from "../user-info/UserInfo.tsx";
 
 type UserProps = {
     user: IUser;
@@ -8,12 +9,16 @@ type UserProps = {
 export const UserAvatar: FC<UserProps> = ({user}) => {
     const {firstName, lastName} = user;
     const avatarLetters = `${firstName.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}`;
+    const [infoIsVisible, setInfoIsVisible] = useState<boolean>(false);
 
     return (
-        <div>
-            <div className='rounded-[50%] bg-indigo-600 text-white w-12 h-12 flex items-center justify-center'>
+        <div className='h-[100%] w-[100px] flex items-center justify-center'
+             onMouseLeave={() => setInfoIsVisible(false)}>
+            <div className='rounded-[50%] bg-indigo-600 text-white w-12 h-12 flex items-center justify-center cursor-pointer'
+                 onClick={() => setInfoIsVisible(!infoIsVisible)}>
                 <p>{avatarLetters}</p>
             </div>
+            {infoIsVisible && <UserInfo user={user} isVisible={infoIsVisible} />}
         </div>
     );
 };
