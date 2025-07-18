50bcd365ea72f7a2db6c9dc6ae3fb4f7113bf3fd
Denys Podobrii <denys.podobrii@gmail.com>
2025-07-18 19:54:54 +0300

    fixed regexp pattern for username

--- src/validators/login-validator.ts
+++ src/validators/login-validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const loginValidator = Joi.object({
-    username: Joi.string().pattern(/[a-zA-Z]+/).required().messages({
+    username: Joi.string().pattern(/[a-zA-Z0-9_-]+$/).required().messages({
         'string.pattern.base': 'Username must be a string',
         'string.empty': 'Username is required',
     }),
